135a05be635d4a8f6c5c0bb010eb823e53dcac32
Imaparadox <stevemarsh117@gmail.com>
2021-06-29 16:29:25 -0400

    Changed file names and added API routes to server.js

---
+++ routes/apiRoutes/notesAPI.js
@@ -0,0 +1,6 @@
+const { notes } = require('/data/db')
+app.get('/data/db', (req, res) => {
+    res.send('Hello!');
+});
+
+module.exports = notes
--- server.js
+++ server.js
@@ -2,6 +2,10 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+// Use apiRoutes
+app.use('/api', apiRoutes);
+app.use('/', htmlRoutes);
+
 app.listen(PORT, () => {
     console.log(`API server now on port ${PORT}!`);
   });
