cb465f07c6c10b447e7cbafe7e49a82bd2809c5e
Imaparadox <stevemarsh117@gmail.com>
2021-06-29 16:32:54 -0400

    changed server.js

--- server.js
+++ server.js
@@ -3,8 +3,8 @@ const app = express();
 const PORT = process.env.PORT || 3001;
 
 // Use apiRoutes
-app.use('/api', apiRoutes);
-app.use('/', htmlRoutes);
+// app.use('/api', apiRoutes);
+// app.use('/', htmlRoutes);
 
 app.listen(PORT, () => {
     console.log(`API server now on port ${PORT}!`);
