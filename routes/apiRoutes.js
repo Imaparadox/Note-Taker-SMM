588cf5d552c41ea4b4d9e6f5cae0772b4ac28d10
Imaparadox <stevemarsh117@gmail.com>
2021-06-30 14:14:19 -0400

    Editted POST route further

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,22 +1,20 @@
 const router = require('express').Router();
 const store = require('../data/store');
 //Deconstructor for notes
-const { notes } = require('../data/notes.json');
+// const { notes } = require('../data/notes.json');
 
 router.get('/notes', (req, res) => {
     store.getNotes().then(notes => { return res.json(notes) })
-    .catch(error => {res.status(500).json(error)})
+        .catch(error => { res.status(500).json(error) })
 });
 
 //POST route
 router.post('/notes', (req, res) => {
-    // res.send('Got a POST request')
-    req.body.id = notes.length.toString();
-     
-
-
+    store.addNote().then(notes => { return res.json(notes) })
+        .catch(error => { res.status(400).json(error) })
 })
-//DELETE
-router
+
+//DELETE route
+
 
 module.exports = router
