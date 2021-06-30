8ac7a50a1857a68e9c65e7b532c8c893ef2f9001
Imaparadox <stevemarsh117@gmail.com>
2021-06-30 13:18:51 -0400

    Began editting post route

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,5 +1,7 @@
 const router = require('express').Router();
 const store = require('../data/store');
+//Deconstructor for notes
+const { notes } = require('../data/notes.json');
 
 router.get('/notes', (req, res) => {
     store.getNotes().then(notes => { return res.json(notes) })
@@ -7,8 +9,14 @@ router.get('/notes', (req, res) => {
 });
 
 //POST route
+router.post('/notes', (req, res) => {
+    // res.send('Got a POST request')
+    req.body.id = notes.length.toString();
+     
 
+
+})
 //DELETE
-
+router
 
 module.exports = router
