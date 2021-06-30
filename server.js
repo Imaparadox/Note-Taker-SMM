20af3bc8b7d22aee3c2ad9027c771b3b69e52a16
Imaparadox <stevemarsh117@gmail.com>
2021-06-30 15:39:27 -0400

    Added middleware

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+//Global scope
 const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -6,9 +7,14 @@ const PORT = process.env.PORT || 3001;
 const apiRoutes = require('./routes/apiRoutes');
 const htmlRoutes = require('./routes/htmlRoutes');
 
-// Use apiRoutes
+//Use apiRoutes
 app.use('/api', apiRoutes);
 app.use('/', htmlRoutes);
+
+//Middleware
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static('public'));
 
 app.listen(PORT, () => {
     console.log(`API server now on port ${PORT}!`);
