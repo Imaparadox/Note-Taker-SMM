b5e0f88527f35b1e1df25c95ae152af54b4c7d70
Imaparadox <stevemarsh117@gmail.com>
2021-07-02 17:31:00 -0400

    Fixed POST route and made general alterations to application files

--- data/store.js
+++ data/store.js
@@ -2,15 +2,15 @@ const util = require('util');
 const fs = require('fs');
 const uuid = require('uuidv1');
 
-const readFile = util.promisify(fs.readFile);
-const writeFile = util.promisify(fs.writeFile);
+const readFileAsync = util.promisify(fs.readFile);
+const writeFileAsync = util.promisify(fs.writeFile);
 
 class Store {
     read() {
-        return readFile('data/notes.json', 'utf-8')
+        return readFileAsync('data/notes.json', 'utf8')
     }
     write(note) {
-        return writeFile('data/notes.json', JSON.stringify(note));
+        return writeFileAsync('data/notes.json', JSON.stringify(note));
     }
     getNotes() {
         return this.read().then((notes) => {
@@ -31,10 +31,55 @@ class Store {
         }
         const newNote = { title, text, id: uuid() }
         return this.getNotes()
-            .then((notes) => { [...notes, newNote] })
-            .then((updatedNotes) => {this.write(updatedNotes)})
+            .then((notes) => [...notes, newNote])
+            .then((updatedNotes) => this.write(updatedNotes))
             .then(() => newNote)
     }
+    // removeNote(id) {
+    //     // Get all notes, remove the note with the given id, write the filtered notes
+    //     return this.getNotes()
+    //         .then((notes) => notes.filter((note) => note.id !== id))
+    //         .then((filteredNotes) => this.write(filteredNotes));
+    // }
 }
 
-module.exports = new Store()
+// class Store {
+//     read() {
+//         return readFileAsync('data/notes.json', 'utf8');
+//     }
+//     write(note) {
+//         return writeFileAsync('data/notes.json', JSON.stringify(note));
+//     }
+//     getNotes() {
+//         return this.read().then((notes) => {
+//             let parsedNotes;
+//             // If notes isn't an array or can't be turned into one, send back a new empty array
+//             try {
+//                 parsedNotes = [].concat(JSON.parse(notes));
+//             } catch (err) {
+//                 parsedNotes = [];
+//             }
+//             return parsedNotes;
+//         });
+//     }
+//     addNote(note) {
+//         const { title, text } = note;
+//         if (!title || !text) {
+//             throw new Error("Note 'title' and 'text' cannot be blank");
+//         }
+//         // Add a unique id to the note using uuid package
+//         const newNote = { title, text, id: uuid() };
+//         // Get all notes, add the new note, write all the updated notes, return the newNote
+//         return this.getNotes()
+//             .then((notes) => [...notes, newNote])
+//             .then((updatedNotes) => this.write(updatedNotes))
+//             .then(() => newNote);
+//     }
+    // removeNote(id) {
+    //     // Get all notes, remove the note with the given id, write the filtered notes
+    //     return this.getNotes()
+    //         .then((notes) => notes.filter((note) => note.id !== id))
+    //         .then((filteredNotes) => this.write(filteredNotes));
+    // }
+
+module.exports = new Store();
--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -3,6 +3,15 @@ const store = require('../data/store');
 //Deconstructor for notes
 // const { notes } = require('../data/notes.json');
 
+//router.get('/notes', (req, res) => {
+//let results = notes;
+// if (req.query) {
+//   results = filterByQuery(req.query, results);
+// }
+// res.json(results);
+// console.log(results);
+//});
+
 router.get('/notes', (req, res) => {
     store.getNotes().then(notes => { return res.json(notes) })
         .catch(error => { res.status(500).json(error) })
@@ -10,11 +19,13 @@ router.get('/notes', (req, res) => {
 
 //POST route
 router.post('/notes', (req, res) => {
-    store.addNote().then(notes => { return res.json(notes) })
-        .catch(error => { res.status(400).json(error) })
+    store.addNote(req.body).then(note => { return res.json(note) })
+        .catch(error => { res.status(500).json(error) })
 })
 
 //DELETE route
 
 
 module.exports = router
+
+//readFileAsync and the writeFileAsync
